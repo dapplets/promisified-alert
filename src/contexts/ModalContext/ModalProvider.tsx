05308afc27c2338e46affc46a9e966cf87f1e9c8
Alexander Sakhaev <alsakhaev@gmail.com>
2023-07-25 17:22:54 +0300

    fix: 'useMemo' is defined but never used

--- src/contexts/ModalContext/ModalProvider.tsx
+++ src/contexts/ModalContext/ModalProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useMemo } from "react";
+import React, { FC, ReactElement } from "react";
 
 import { ModalContext, ModalContextState, ModalProps } from "./ModalContext";
 
